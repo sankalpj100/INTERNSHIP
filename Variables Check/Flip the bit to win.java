590278eba19f8cf2898605806dc890f098b4e66e
sankalpj100 <63203517+sankalpj100@users.noreply.github.com>
2020-07-30 23:29:23 +0530

    Update Flip the bit to win.java

--- Variables Check/Flip the bit to win.java
+++ Variables Check/Flip the bit to win.java
@@ -1,3 +1,10 @@
+/*You have an integer and you can flip exactly one bit from a 0 to a 1, Write code to
+find the length of the longest sequence of Is you could create.
+EXAMPLE
+Input: 1775
+Output: 8
+(or:
+11011101111)*/
 class FB {
 	public static int flipTheBit(int inputInteger) {		
 		int onesCurrLength = 0;
